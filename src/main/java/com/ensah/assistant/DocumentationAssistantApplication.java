1223688e04e235e80583ee831e7f68d65df17dac
OUSSAMA EL ALAOUI EL ISMAILI <oussama.elalaouielismali@etu.uae.ac.ma>
2025-02-20 21:28:41 +0100

    Initial commit

---
+++ src/main/java/com/ensah/assistant/DocumentationAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.ensah.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentationAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentationAssistantApplication.class, args);
+	}
+
+}
